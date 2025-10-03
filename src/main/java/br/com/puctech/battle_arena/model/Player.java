18003044badffd91431979b8dc36e908f4f17e42
LuisC <luis.coelho.761@gmail.com>
2025-10-03 16:37:39 -0300

    Criando o model de jogador e Repositório de jogadores

---
+++ src/main/java/br/com/puctech/battle_arena/model/Player.java
@@ -0,0 +1,32 @@
+package br.com.puctech.battle_arena.model;
+
+public class Player {
+
+    private Long id;
+    private String nome;
+    private Integer vida;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getVida() {
+        return vida;
+    }
+
+    public void setVida(Integer vida) {
+        this.vida = vida;
+    }
+}
