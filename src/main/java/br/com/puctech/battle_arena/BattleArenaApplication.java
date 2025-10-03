1a2710b1a3bd076341401e0572b3cc87b783155a
LuisC <luis.coelho.761@gmail.com>
2025-10-03 16:03:31 -0300

    Iniciado o projeto battle-arena

---
+++ src/main/java/br/com/puctech/battle_arena/BattleArenaApplication.java
@@ -0,0 +1,13 @@
+package br.com.puctech.battle_arena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleArenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/puctech/battle_arena/BattleArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.puctech.battle_arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
