32744da1fc9cd1fdf0cd9029b959597f03081bf6
LuisC <luis.coelho.761@gmail.com>
2025-10-04 21:42:59 -0300

    Terminando parte-1 do jogo

--- src/main/java/br/com/puctech/battle_arena/controller/PlayerController.java
+++ src/main/java/br/com/puctech/battle_arena/controller/PlayerController.java
@@ -1,37 +1,62 @@
 package br.com.puctech.battle_arena.controller;
 
 import br.com.puctech.battle_arena.model.Player;
+import br.com.puctech.battle_arena.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/game")
 public class PlayerController {
 
+    @Autowired
+    PlayerService playerService;
+
     @GetMapping("/findAll")
     public ResponseEntity<List<Player>> getAll() {
-        return ResponseEntity.ok(null);
+
+        List<Player> playerList = playerService.getPlayers();
+
+        return ResponseEntity.ok().body(playerList);
     }
 
     @GetMapping("/{playerId}")
     public ResponseEntity<Player> getByID(@PathVariable Long playerId) {
-        return ResponseEntity.ok(null);
+        Optional<Player> player = playerService.getPlayerById(playerId);
+
+        if (player.isPresent()) {
+            return ResponseEntity.ok().body(player.get());
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping("/create_user")
     public ResponseEntity<Player> createPlayer(@RequestBody Player player) {
-        return ResponseEntity.ok(null);
+        Player savedPlayer = playerService.createPlayer(player);
+
+        return ResponseEntity.ok(savedPlayer);
     }
 
     @PutMapping("/attack/{id}")
     public ResponseEntity<String> attackPlayer(@PathVariable Long id) {
-        return ResponseEntity.ok(null);
+        try {
+            return ResponseEntity.ok(playerService.attackPlayer(id));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PutMapping("/heal/{id}")
     public ResponseEntity<String> healPlayer(@PathVariable Long id) {
-        return ResponseEntity.ok(null);
+        try {
+            return ResponseEntity.ok(playerService.healPlayer(id));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
